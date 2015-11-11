63d8d2ae3e7e4c6ff781afa9b03418daa7e2c2f4
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2015-11-12 00:22:49 +0600

    Created Attendance model.

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,3 +1,5 @@
 class Period < ActiveRecord::Base
   belongs_to :course_element
+  has_many :attendances
+  has_many :students, through: :attendances
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ActiveRecord::Base
   has_many :group_memberships
   has_many :groups, through: :group_memberships
+
+  has_many :attendances
+  has_many :periods, through: :attendances
 end
