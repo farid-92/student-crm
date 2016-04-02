ab022dff5b42384b2d8c5b5b15cb1a6d016e0722
Farid <farid.babazov@gmail.com>
2016-04-02 16:43:40 +0600

    Add devise to users, and add some additional column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111181953) do
+ActiveRecord::Schema.define(version: 20160402102133) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer  "student_id"
@@ -72,12 +72,33 @@ ActiveRecord::Schema.define(version: 20151111181953) do
 
   add_index "periods", ["course_element_id"], name: "index_periods_on_course_element_id"
 
-  create_table "students", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "name"
-    t.string   "phone"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "surname"
+    t.string   "gender"
+    t.string   "first_phone"
+    t.string   "second_phone"
+    t.string   "skype"
+    t.date     "birthdate"
+    t.string   "passport_id"
+    t.string   "passport_inn"
+    t.date     "issue_date"
+    t.string   "issued_by"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
