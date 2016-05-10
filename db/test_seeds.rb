b48379b39ff01f4aa6beefa21bd3b48e08e0691c
Farid <farid.babazov@gmail.com>
2016-05-10 18:38:07 +0600

    Изменил фикстуры

--- db/test_seeds.rb
+++ db/test_seeds.rb
@@ -2,10 +2,16 @@ password = '12345678'
 admin = User.create!(name: 'Фарид',
                      surname: 'Бабазов',
                      gender: 'Мужчина',
-                     birthdate: '06.11.1992',
-                     first_phone: '+996772183644',
-                     second_phone: '+996550362180',
                      skype: 'farid.babazov',
                      email: 'admin@gmail.com',
+                     birthdate: '06.11.1992',
+                     gender: 'мужской',
+                     first_phone: '+996772183644',
+                     second_phone: '+996550362180',
+                     passport_id: '123123',
+                     passport_inn: '100101020',
+                     issue_date: '20.12.2008',
+                     issued_by: 'MVD 50-05',
                      password: password,
-                     password_confirmation: password,)
+                     password_confirmation: password
+)
