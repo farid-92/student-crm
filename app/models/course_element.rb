b39a330624175106ff822035a0fa2d793aec9d12
Farid <farid.babazov@gmail.com>
2016-05-13 18:00:42 +0600

    #8 Добавил связи между моделями

--- app/models/course_element.rb
+++ app/models/course_element.rb
@@ -1,6 +1,8 @@
 class CourseElement < ActiveRecord::Base
   belongs_to :course
-  has_many :periods
+  has_many :periods, dependent: :destroy
+  has_many :course_element_materials, dependent: :destroy
+
 
   validates :theme, :element_type, :course_id, presence: true
 end
--- app/models/course_element_material.rb
+++ app/models/course_element_material.rb
@@ -1,2 +1,7 @@
 class CourseElementMaterial < ActiveRecord::Base
+
+  belongs_to :course_element
+
+  validates :content, :title, :element_type, presence: true
+
 end
