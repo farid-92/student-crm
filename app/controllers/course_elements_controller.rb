ddead2d5339abe2221cd7c3a12afea8e382985ed
Farid <farid.babazov@gmail.com>
2016-05-13 17:41:45 +0600

    #7 Добавил возможность добавлять элементы курса(раздатка)

--- app/controllers/course_elements_controller.rb
+++ app/controllers/course_elements_controller.rb
@@ -1,2 +1,26 @@
 class CourseElementsController < ApplicationController
+  def new
+    @course_element = CourseElement.new
+    @course = Course.find(params[:course_id])
+  end
+
+  def create
+    @course_element = CourseElement.new(get_course_element_params)
+    if @course_element.save
+      flash[:success] = 'Вы успешно создали тему'
+
+      redirect_to show_course_index_url(@course_element.course, resource: 3)
+    else
+      @course = @course_element.course
+      render action: 'new'
+    end
+  end
+
+
+  private
+
+  def get_course_element_params
+    params.require(:course_element).permit(:theme, :element_type, :course_id)
+  end
+
 end
