8e9052a854675f0e4ddea72935aa39bc312d0435
Farid <farid.babazov@gmail.com>
2016-05-14 14:29:18 +0600

    #9 Добавил связь между таблицами

--- app/models/course_element.rb
+++ app/models/course_element.rb
@@ -2,7 +2,7 @@ class CourseElement < ActiveRecord::Base
   belongs_to :course
   has_many :periods, dependent: :destroy
   has_many :course_element_materials, dependent: :destroy
-
+  has_many :course_element_files, dependent: :destroy
 
   validates :theme, :element_type, :course_id, presence: true
 end
--- app/models/course_element_file.rb
+++ app/models/course_element_file.rb
@@ -1,2 +1,11 @@
 class CourseElementFile < ActiveRecord::Base
+  include Paperclip::Glue
+
+  belongs_to :course_element
+
+  has_attached_file :file
+
+  do_not_validate_attachment_file_type :file
+
+
 end
