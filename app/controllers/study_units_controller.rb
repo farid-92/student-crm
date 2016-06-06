8c035ef2459e249a3e539f5ab7cf2627e3480b96
Farid <farid.babazov@gmail.com>
2016-06-06 14:59:56 +0600

    #13 Создал контроллер StudyUnits

---
+++ app/controllers/study_units_controller.rb
@@ -0,0 +1,2 @@
+class StudyUnitsController < ApplicationController
+end
---
+++ test/controllers/study_units_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StudyUnitsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
