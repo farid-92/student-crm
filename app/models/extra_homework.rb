d6ccd214c41d1debb9493639baa637ffabb3f209
Farid <farid.babazov@gmail.com>
2016-06-09 15:24:48 +0600

    #15 Создал модель ExtraHomework

---
+++ app/models/extra_homework.rb
@@ -0,0 +1,2 @@
+class ExtraHomework < ActiveRecord::Base
+end
---
+++ db/migrate/20160609092306_create_extra_homeworks.rb
@@ -0,0 +1,16 @@
+class CreateExtraHomeworks < ActiveRecord::Migration
+  def change
+    create_table :extra_homeworks do |t|
+      t.references :group, index: true, foreign_key: true
+      t.references :student, index: true, foreign_key: true
+      t.references :period, index: true, foreign_key: true
+      t.references :course, index: true, foreign_key: true
+      t.references :homework, foreign_key: true
+      t.text :feedback
+      t.boolean :download_status, default: false
+      t.string :teacher_id, default: false
+
+      t.timestamps null: false
+    end
+  end
+end
