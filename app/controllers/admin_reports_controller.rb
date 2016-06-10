59f83b8d52668880e795fa51473f00aea60849b3
Farid <farid.babazov@gmail.com>
2016-06-10 16:17:51 +0600

    #15 Добавил вьюшки для отображения ведомости
    Куча вьюшек было добавлено, Так же добавил экшины в контроллеры admin_reports и teacher_reports

--- app/controllers/admin_reports_controller.rb
+++ app/controllers/admin_reports_controller.rb
@@ -1,3 +1,75 @@
 class AdminReportsController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+  # def get_course_groups
+  #   course_id = params[:course_id]
+  #
+  #   course = Course.find_by(id: course_id)
+  #   course_groups = course.groups
+  #
+  #   groups_list = []
+  #   course_groups.each do |group|
+  #     groups_list.push({id: group.id, name: group.name})
+  #   end
+  #
+  #   render json: groups_list
+  # end
+
+  def attendance_current_study_unit
+    current_study_unit
+    render_admin_attendance_table
+  end
+
+  def homework_current_study_unit
+    current_study_unit
+    render_admin_homework_table
+  end
+
+  def attendance_previous_study_unit
+    previous_study_unit
+    render_admin_attendance_table
+  end
+
+  def homework_previous_study_unit
+    previous_study_unit
+    render_admin_homework_table
+  end
+
+  def attendance_report_table
+    get_group_report_table
+    render_admin_attendance_table
+  end
+
+  def homework_report_table
+    get_group_report_table
+    render_admin_homework_table
+  end
+
+  def admin_reports_filters
+    get_group_filters
+    render partial: 'admin_reports/admin_reports_filters'
+  end
+
+  def attendance_educational_unit
+    get_educational_unit
+    render_admin_attendance_table
+  end
+
+  def homework_educational_unit
+    get_educational_unit
+    render_admin_homework_table
+  end
+
+  private
+
+  def render_admin_attendance_table
+    render partial: 'admin_reports/admin_attendances_table'
+  end
+
+  def render_admin_homework_table
+    render partial: 'shared/homeworks_table'
+  end
 
 end
--- app/helpers/teacher_reports_helper.rb
+++ app/helpers/teacher_reports_helper.rb
@@ -1,2 +1,67 @@
 module TeacherReportsHelper
+  def attended?(period_id, student_id)
+    attendance = Attendance.find_by_period_id_and_user_id(period_id, student_id)
+    if attendance.attended
+      true
+    else
+      false
+    end
+  end
+
+  def show_student_total_attendance(student_id)
+    periods = []
+
+    student = User.find(student_id)
+    periods += student.periods.to_a
+
+    Attendance.attended_periods(student_id, periods)
+  end
+
+  def show_period_attendance(students, period)
+    Attendance.attended_periods(students, period)
+  end
+
+  def show_next_period(periods, selected_period)
+    sorted_periods = periods.sort_by do |period|
+      period.commence_datetime
+    end
+
+    future_periods = []
+    sorted_periods.each do |period|
+      future_periods.push period if period.commence_datetime.strftime('%d.%m.%Y').to_date > Date.today
+    end
+
+    'info' if future_periods[0] == selected_period
+  end
+
+  def show_disabled_toggle(period)
+    'disabled' if period.commence_datetime.strftime('%d.%m.%Y').to_date > Date.today
+  end
+
+  def show_homework_stats(object)
+    student_hw = []
+    sent_hw = []
+    checked_hw = []
+    get_students_array(object, student_hw)
+    get_stats(checked_hw, sent_hw, student_hw)
+  end
+
+  def get_students_array(object, object_array)
+    object.homeworks.where(group_id: @group).each do |homework|
+      # homework.student.admissions.each do |admission|
+      #   if admission.role.access == 'student'
+          object_array.push homework
+        # end
+      # end
+    end
+  end
+
+  def get_stats(checked_hw, sent_hw, object_array)
+    object_array.each do |homework|
+      sent_hw.push homework if homework.score.to_i > 0
+      checked_hw.push homework if homework.score.to_i > 1
+    end
+    "#{sent_hw.count}/#{checked_hw.count}"
+  end
+
 end
