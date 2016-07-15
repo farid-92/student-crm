2b10c3dc3e3ee3f5120ac9683de724cddaddd58e
Farid <farid.babazov@gmail.com>
2016-07-15 17:20:42 +0600

    Изменил дату миграции для некоторых миграций
    Сделано это было потому что postgresql не позволяет добавить внешний ключ несуществующей таблицы к таблице

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160620100339) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "attendances", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "period_id"
@@ -21,8 +24,8 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "attendances", ["period_id"], name: "index_attendances_on_period_id"
-  add_index "attendances", ["user_id"], name: "index_attendances_on_user_id"
+  add_index "attendances", ["period_id"], name: "index_attendances_on_period_id", using: :btree
+  add_index "attendances", ["user_id"], name: "index_attendances_on_user_id", using: :btree
 
   create_table "contact_lists", force: :cascade do |t|
     t.string   "title"
@@ -41,7 +44,7 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.datetime "file_updated_at"
   end
 
-  add_index "course_element_files", ["course_element_id"], name: "index_course_element_files_on_course_element_id"
+  add_index "course_element_files", ["course_element_id"], name: "index_course_element_files_on_course_element_id", using: :btree
 
   create_table "course_element_materials", force: :cascade do |t|
     t.string   "title"
@@ -52,7 +55,7 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.datetime "updated_at",        null: false
   end
 
-  add_index "course_element_materials", ["course_element_id"], name: "index_course_element_materials_on_course_element_id"
+  add_index "course_element_materials", ["course_element_id"], name: "index_course_element_materials_on_course_element_id", using: :btree
 
   create_table "course_elements", force: :cascade do |t|
     t.integer  "course_id"
@@ -62,7 +65,7 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.datetime "updated_at",   null: false
   end
 
-  add_index "course_elements", ["course_id"], name: "index_course_elements_on_course_id"
+  add_index "course_elements", ["course_id"], name: "index_course_elements_on_course_id", using: :btree
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -81,7 +84,7 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.datetime "updated_at",      null: false
   end
 
-  add_index "custom_lists", ["contact_list_id"], name: "index_custom_lists_on_contact_list_id"
+  add_index "custom_lists", ["contact_list_id"], name: "index_custom_lists_on_contact_list_id", using: :btree
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -109,10 +112,10 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.datetime "extra_hw_updated_at"
   end
 
-  add_index "extra_homeworks", ["course_id"], name: "index_extra_homeworks_on_course_id"
-  add_index "extra_homeworks", ["group_id"], name: "index_extra_homeworks_on_group_id"
-  add_index "extra_homeworks", ["period_id"], name: "index_extra_homeworks_on_period_id"
-  add_index "extra_homeworks", ["user_id"], name: "index_extra_homeworks_on_user_id"
+  add_index "extra_homeworks", ["course_id"], name: "index_extra_homeworks_on_course_id", using: :btree
+  add_index "extra_homeworks", ["group_id"], name: "index_extra_homeworks_on_group_id", using: :btree
+  add_index "extra_homeworks", ["period_id"], name: "index_extra_homeworks_on_period_id", using: :btree
+  add_index "extra_homeworks", ["user_id"], name: "index_extra_homeworks_on_user_id", using: :btree
 
   create_table "group_memberships", force: :cascade do |t|
     t.integer  "group_id"
@@ -122,8 +125,8 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "group_memberships", ["group_id"], name: "index_group_memberships_on_group_id"
-  add_index "group_memberships", ["user_id"], name: "index_group_memberships_on_user_id"
+  add_index "group_memberships", ["group_id"], name: "index_group_memberships_on_group_id", using: :btree
+  add_index "group_memberships", ["user_id"], name: "index_group_memberships_on_user_id", using: :btree
 
   create_table "groups", force: :cascade do |t|
     t.integer  "course_id"
@@ -135,7 +138,7 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.datetime "updated_at",       null: false
   end
 
-  add_index "groups", ["course_id"], name: "index_groups_on_course_id"
+  add_index "groups", ["course_id"], name: "index_groups_on_course_id", using: :btree
 
   create_table "homeworks", force: :cascade do |t|
     t.integer  "course_id"
@@ -154,10 +157,10 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.datetime "hw_updated_at"
   end
 
-  add_index "homeworks", ["course_id"], name: "index_homeworks_on_course_id"
-  add_index "homeworks", ["group_id"], name: "index_homeworks_on_group_id"
-  add_index "homeworks", ["period_id"], name: "index_homeworks_on_period_id"
-  add_index "homeworks", ["user_id"], name: "index_homeworks_on_user_id"
+  add_index "homeworks", ["course_id"], name: "index_homeworks_on_course_id", using: :btree
+  add_index "homeworks", ["group_id"], name: "index_homeworks_on_group_id", using: :btree
+  add_index "homeworks", ["period_id"], name: "index_homeworks_on_period_id", using: :btree
+  add_index "homeworks", ["user_id"], name: "index_homeworks_on_user_id", using: :btree
 
   create_table "periods", force: :cascade do |t|
     t.integer  "course_element_id"
@@ -173,10 +176,10 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.integer  "study_unit_id"
   end
 
-  add_index "periods", ["course_element_id"], name: "index_periods_on_course_element_id"
-  add_index "periods", ["course_id"], name: "index_periods_on_course_id"
-  add_index "periods", ["group_id"], name: "index_periods_on_group_id"
-  add_index "periods", ["study_unit_id"], name: "index_periods_on_study_unit_id"
+  add_index "periods", ["course_element_id"], name: "index_periods_on_course_element_id", using: :btree
+  add_index "periods", ["course_id"], name: "index_periods_on_course_id", using: :btree
+  add_index "periods", ["group_id"], name: "index_periods_on_group_id", using: :btree
+  add_index "periods", ["study_unit_id"], name: "index_periods_on_study_unit_id", using: :btree
 
   create_table "recipient_depositories", force: :cascade do |t|
     t.integer  "user_id"
@@ -185,8 +188,8 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.datetime "updated_at",      null: false
   end
 
-  add_index "recipient_depositories", ["contact_list_id"], name: "index_recipient_depositories_on_contact_list_id"
-  add_index "recipient_depositories", ["user_id"], name: "index_recipient_depositories_on_user_id"
+  add_index "recipient_depositories", ["contact_list_id"], name: "index_recipient_depositories_on_contact_list_id", using: :btree
+  add_index "recipient_depositories", ["user_id"], name: "index_recipient_depositories_on_user_id", using: :btree
 
   create_table "senders", force: :cascade do |t|
     t.string   "name"
@@ -195,7 +198,7 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.datetime "updated_at",             null: false
   end
 
-  add_index "senders", ["sms_service_account_id"], name: "index_senders_on_sms_service_account_id"
+  add_index "senders", ["sms_service_account_id"], name: "index_senders_on_sms_service_account_id", using: :btree
 
   create_table "sms_deliveries", force: :cascade do |t|
     t.string   "title"
@@ -209,8 +212,8 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.boolean  "smart_delivery",  default: false
   end
 
-  add_index "sms_deliveries", ["contact_list_id"], name: "index_sms_deliveries_on_contact_list_id"
-  add_index "sms_deliveries", ["sender_id"], name: "index_sms_deliveries_on_sender_id"
+  add_index "sms_deliveries", ["contact_list_id"], name: "index_sms_deliveries_on_contact_list_id", using: :btree
+  add_index "sms_deliveries", ["sender_id"], name: "index_sms_deliveries_on_sender_id", using: :btree
 
   create_table "sms_service_accounts", force: :cascade do |t|
     t.string   "login"
@@ -220,18 +223,16 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.integer  "user_id"
   end
 
-  add_index "sms_service_accounts", ["user_id"], name: "index_sms_service_accounts_on_user_id"
+  add_index "sms_service_accounts", ["user_id"], name: "index_sms_service_accounts_on_user_id", using: :btree
 
   create_table "study_units", force: :cascade do |t|
     t.string   "unit"
-    t.integer  "periods_id"
     t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "study_units", ["group_id"], name: "index_study_units_on_group_id"
-  add_index "study_units", ["periods_id"], name: "index_study_units_on_periods_id"
+  add_index "study_units", ["group_id"], name: "index_study_units_on_group_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                       default: "", null: false
@@ -268,7 +269,36 @@ ActiveRecord::Schema.define(version: 20160620100339) do
     t.string   "password_txt"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "attendances", "periods"
+  add_foreign_key "attendances", "users"
+  add_foreign_key "course_element_files", "course_elements"
+  add_foreign_key "course_element_materials", "course_elements"
+  add_foreign_key "course_elements", "courses"
+  add_foreign_key "custom_lists", "contact_lists"
+  add_foreign_key "extra_homeworks", "courses"
+  add_foreign_key "extra_homeworks", "groups"
+  add_foreign_key "extra_homeworks", "homeworks"
+  add_foreign_key "extra_homeworks", "periods"
+  add_foreign_key "extra_homeworks", "users"
+  add_foreign_key "group_memberships", "groups"
+  add_foreign_key "group_memberships", "users"
+  add_foreign_key "groups", "courses"
+  add_foreign_key "homeworks", "courses"
+  add_foreign_key "homeworks", "groups"
+  add_foreign_key "homeworks", "periods"
+  add_foreign_key "homeworks", "users"
+  add_foreign_key "periods", "course_elements"
+  add_foreign_key "periods", "courses"
+  add_foreign_key "periods", "groups"
+  add_foreign_key "periods", "study_units"
+  add_foreign_key "recipient_depositories", "contact_lists"
+  add_foreign_key "recipient_depositories", "users"
+  add_foreign_key "senders", "sms_service_accounts"
+  add_foreign_key "sms_deliveries", "contact_lists"
+  add_foreign_key "sms_deliveries", "senders"
+  add_foreign_key "sms_service_accounts", "users"
+  add_foreign_key "study_units", "groups"
 end
