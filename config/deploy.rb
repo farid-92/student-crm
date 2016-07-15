4a5fd3ab2820973abd92112b780b4aecc57ebc22
Farid <farid.babazov@gmail.com>
2016-07-15 17:35:17 +0600

    Изменил deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,7 +74,7 @@ namespace :deploy do
 
   desc 'reload the database with seed data'
   task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; bundle exec rake db:seed"
   end
 
   before :starting,     :check_revision
