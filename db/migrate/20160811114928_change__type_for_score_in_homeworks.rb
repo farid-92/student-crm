353d735e5001aa7e77a9b6be23d91635083e8f9f
Farid <farid.babazov@gmail.com>
2016-08-23 14:30:03 +0600

    #21 Изменил миграция для mysql

--- db/migrate/20160811114928_change__type_for_score_in_homeworks.rb
+++ db/migrate/20160811114928_change__type_for_score_in_homeworks.rb
@@ -1,5 +1,5 @@
 class ChangeTypeForScoreInHomeworks < ActiveRecord::Migration
   def change
-    change_column(:homeworks, :score, 'decimal USING CAST(score AS decimal)')
+    change_column(:homeworks, :score, :decimal)
   end
 end
