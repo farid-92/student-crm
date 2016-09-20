313ae1fc0d7b320ff52d49ed398d6883306cb425
Farid <farid.babazov@gmail.com>
2016-09-20 14:45:36 +0600

    #23 Создал миграцию в которой создается модель role и настройка с моделью user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824120252) do
+ActiveRecord::Schema.define(version: 20160920084144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -207,6 +207,17 @@ ActiveRecord::Schema.define(version: 20160824120252) do
   add_index "recipient_depositories", ["contact_list_id"], name: "index_recipient_depositories_on_contact_list_id", using: :btree
   add_index "recipient_depositories", ["user_id"], name: "index_recipient_depositories_on_user_id", using: :btree
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
+
   create_table "senders", force: :cascade do |t|
     t.string   "name"
     t.integer  "sms_service_account_id"
@@ -288,6 +299,13 @@ ActiveRecord::Schema.define(version: 20160824120252) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
   add_foreign_key "attendances", "periods"
   add_foreign_key "attendances", "users"
