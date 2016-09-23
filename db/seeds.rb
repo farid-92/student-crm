1e580da3302bcd50f4c7d844afca3dfd91bbf211
Altynbek Abdrakmanov <altiwbek@gmail.com>
2016-09-23 16:47:32 +0600

    #21 disable seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,23 +17,23 @@
 #                      password_txt: password
 # )
 
-password = '12345678'
-admin = User.create!(name: 'Altyn',
-                     surname: 'Altynych',
-                     skype: 'altyn.alt',
-                     email: 'altynych@gmail.com',
-                     birthdate: '06.11.1992',
-                     gender: 'Мужчина',
-                     first_phone: '+996772183644',
-                     second_phone: '+996550362180',
-                     passport_id: '123123',
-                     passport_inn: '100101020',
-                     issue_date: '20.12.2008',
-                     issued_by: 'MVD 50-05',
-                     password: password,
-                     password_confirmation: password,
-                     password_txt: password
-)
+# password = '12345678'
+# admin = User.create!(name: 'Altyn',
+#                      surname: 'Altynych',
+#                      skype: 'altyn.alt',
+#                      email: 'altynych@gmail.com',
+#                      birthdate: '06.11.1992',
+#                      gender: 'Мужчина',
+#                      first_phone: '+996772183644',
+#                      second_phone: '+996550362180',
+#                      passport_id: '123123',
+#                      passport_inn: '100101020',
+#                      issue_date: '20.12.2008',
+#                      issued_by: 'MVD 50-05',
+#                      password: password,
+#                      password_confirmation: password,
+#                      password_txt: password
+# )
 
 # #
 # 20.times do
