44fe31dc3271b61bf13cff54e429381677d9ced6
Farid <farid.babazov@gmail.com>
2016-09-23 14:42:15 +0600

    #21 Изменил тип поля для score на decimal

--- db/migrate/20160811114928_change__type_for_score_in_homeworks.rb
+++ db/migrate/20160811114928_change__type_for_score_in_homeworks.rb
@@ -1,5 +1,5 @@
 class ChangeTypeForScoreInHomeworks < ActiveRecord::Migration
   def change
-    change_column(:homeworks, :score, 'decimal USING CAST(score AS decimal)') # for postgers use 'decimal USING CAST(score AS decimal)'
+    change_column(:homeworks, :score, 'decimal') # for postgers use 'decimal USING CAST(score AS decimal)'
   end
 end
