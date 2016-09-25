849dce4a2e6cf11de8d3b5b09306b685daaf7131
Altynbek Abdrakmanov <altiwbek@gmail.com>
2016-09-25 15:02:57 +0600

    production: config.assets.initialize_on_precompile = false

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,9 +1,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-
-  config.assets.initialize_on_precompile = false
-
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
